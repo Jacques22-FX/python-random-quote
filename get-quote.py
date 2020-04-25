ce082ca6172db751d99346e597952192fce47502
Jacques22-FX <64321066+Jacques22-FX@users.noreply.github.com>
2020-04-25 21:12:45 +0200

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -2,11 +2,11 @@
 def main():
   #print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+ f = open("quotes.txt")
+ quotes = f.readlines()
+ f.close()
 
-  print(quotes[0])
+ print(quotes[0])
 
 if __name__== "__main__":
   main()
